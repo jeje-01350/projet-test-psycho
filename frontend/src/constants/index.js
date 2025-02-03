54d68bd006cb70b06ed35db8e72811356568513b
Jeremy <jeremy@sensei-france.fr>
2025-02-03 15:16:54 +0100

    chore: cleanup constants and ignore PDF files

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,5 +1,4 @@
 import questions from "./Career-Prediction/data.js";
-import personalityTestQuestion from "./Personality/data.js";
 import riasecQuestions from "./Riasec/data.js";
 import bigfiveQuestions from "./Bigfive/data.js";
 import mindFlareQuestions from "./mindFlare/data.js";
@@ -10,4 +9,4 @@ import papiQuestions from "./Papi/data.js";
 import mbti_personalities from "./mbti";
 import ancreScheinQuestions from "./AncreSchein/data.js";
 
-export { questions, mbti_personalities, personalityTestQuestion, kapableQuestions, ancreScheinQuestions, riasecQuestions, bigfiveQuestions, mindFlareQuestions, DjangoQuestions, mbtiQuestions, papiQuestions };
+export { questions, mbti_personalities, kapableQuestions, ancreScheinQuestions, riasecQuestions, bigfiveQuestions, mindFlareQuestions, DjangoQuestions, mbtiQuestions, papiQuestions };
