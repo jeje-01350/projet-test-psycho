373088716f76965185cb19dd52c61b262c6d02b7
Jeremy <jeremy@sensei-france.fr>
2025-02-17 10:07:40 +0100

    refactor: remove deprecated personality test models, controllers, and routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,24 +1,10 @@
 const express = require("express");
-const personalityTestRoutes = require('./personalityTest');
-const bigFiveRoutes = require('./bigFive');
-const resilienceRoutes = require('./resilience');
-const emotionalIntelligenceRoutes = require('./emotionalIntelligence');
-const discRoutes = require('./discRoutes');
-const hexacoRoutes = require('./hexacoRoutes');
 const ambiRoutes = require('./ambiRoutes');
 const gcbsRoutes = require('./gcbsRoutes');
 const router = express.Router();
 
-
-router.use("/personality-test", personalityTestRoutes);
-router.use("/bigfive", bigFiveRoutes);
-router.use("/resilience", resilienceRoutes);
-router.use("/emotional-intelligence", emotionalIntelligenceRoutes);
-router.use("/disc", discRoutes);
-router.use("/hexaco", hexacoRoutes);
 router.use("/ambi", ambiRoutes);
 router.use("/gcbs", gcbsRoutes);
-router.use("/call-modjo", require("./callModjo"));
 
 
 
--- backend/routes/personalityTest.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const personalityTestController = require('../controllers/personalityTestController');
-
-router.post('/save', personalityTestController.savePersonalityTestResult);
-router.get('/checkHsObjectId/:hs_object_id', personalityTestController.checkHsObjectId);
-router.post('/hubspot', personalityTestController.saveHubspotTest);
-
-module.exports = router;
